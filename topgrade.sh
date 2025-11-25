35fbc2239d851365f64aaa57b309966a21b87274
acebmxer <nick.pozza1@gmail.com>
2025-11-25 14:20:06 -0500

    Update topgrade.sh

--- topgrade.sh
+++ topgrade.sh
@@ -66,7 +66,7 @@ cd dotfiles
 ./install.sh
 chsh -s /bin/zsh
 EOF
-infosudo "Back to regular user."
+info "Back to regular user."
 # ────────────────────────────────────────────────────────
 # 7️⃣ System pre‑upgrade (optional but handy)
 # ────────────────────────────────────────────────────────
